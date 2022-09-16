b1a735701fa37c344fcdaa30194dd02c41c1cf84
Andreas Eberli <andreas@eberli.me>
2022-09-16 20:33:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
  <head>
   <title>Header</title>
+  <script async defer data-website-id="d81e9724-76ad-489f-a60e-d965d8c39339" src="https://umami.eberli.me/umami.js"></script>
  </head>
  <body>
  <?php 
